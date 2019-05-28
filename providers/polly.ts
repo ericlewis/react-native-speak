9bafcda2ec2a3555ad5a1de90800c67fb9a86a96
Eric Lewis <ericlewis777@gmail.com>
2019-05-28 15:15:20 -0400

    Polly integration
    This is untested, but I think it should work.

---
+++ providers/polly.ts
@@ -0,0 +1,43 @@
+import Polly from 'aws-sdk/clients/polly';
+import { Provider } from './base';
+
+export class PollyProvider extends Provider {
+  private polly: Polly;
+
+  constructor(accessKeyId: string, secretAccessKey: string) {
+    super(secretAccessKey);
+
+    this.polly = new Polly({
+      signatureVersion: 'v4',
+      region: 'us-east-1',
+      accessKeyId,
+      secretAccessKey
+    });
+  }
+
+  public getVoices = async (): Promise<any> => {
+    return this.polly.describeVoices();
+  };
+
+  public getAudioContent = async (utterance: string) => {
+    return new Promise((resolve, reject) => {
+      this.polly.synthesizeSpeech(
+        {
+          Text: utterance,
+          OutputFormat: 'pcm',
+          VoiceId: 'Kimberly'
+        },
+        (err, data) => {
+          if (err) {
+            reject(err);
+          } else if (data) {
+            if (data.AudioStream) {
+              const buf = data.AudioStream.toString('base64');
+              resolve(buf);
+            }
+          }
+        }
+      );
+    });
+  };
+}
